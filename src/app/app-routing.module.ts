cde567674506babf50240e1d3b55084fa49e12dc
julienfr1 <julienfarhan@farhan.com>
2022-07-06 11:30:09 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdresseComponent } from './composants/adresse/adresse.component';
+import { HomeComponent } from './composants/home/home.component';
+import { NotFoundComponent } from './composants/not-found/not-found.component';
+import { StagiaireComponent } from './composants/stagiaire/stagiaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'adresse', component: AdresseComponent },
+  { path: 'stagiaire', component: StagiaireComponent },
+  { path: 'error', component: NotFoundComponent },
+  { path: '**', redirectTo: '/error' }  //pour tous les autres pages//elle doit etre la derniere
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,6 @@ import { Stagiaire } from './interfaces/stagiaire';
 export class AppComponent {
   title = 'formation-angular';
   
-  tab: number[] = [2, 5, 3, 8];
-
-  moyennes: number[] = [12, 5, 16, 18];
-
-  stagiaire: Stagiaire = { num: 100, nom: "wick", prenom: "john" }
-
-  stagiaires : Stagiaire[] = [
-    { num: 100, nom: "wick", prenom: "john" },
-    { num: 200, nom: "dalton", prenom: "jack" },
-    { num: 300, nom: "doe", prenom: "joe" },
-    { num: 400, nom: "maggio", prenom: "sophie" },
-  ]
-
-  direBonjour() {
-    alert('bonjour');
-  }
+  
 
 }
